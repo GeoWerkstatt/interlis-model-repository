5cb31ae67ad73dccdb3496d5e1327f14b1ceb9fb
Philipp Luethi <philipp.luethi@geowerkstatt.ch>
2023-01-31 15:56:39 +0100

    Add repository-validator container

---
+++ repository-validator/ili2c-repository-check.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+proxy_port=$(echo $PROXY | grep -Eo '[0-9]+' | tail -1)
+proxy_host=${PROXY%":$(echo ${PROXY##*:})"} # remove port
+proxy_host=${proxy_host#*://} # remove protocol
+
+[[ -n $ILIVALIDATOR_MODEL_DIR ]] && options+=" --modeldir $ILIVALIDATOR_MODEL_DIR"
+[[ -n $proxy_host ]] && options+=" --proxy $proxy_host"
+[[ -n $proxy_port ]] && options+=" --proxyPort $proxy_port"
+[[ $ILIVALIDATOR_ENABLE_TRACE = true ]] && options+=" --trace"
+
+# Print executed commands to the Docker container log output
+exec {BASH_XTRACEFD}> >(sudo tee /proc/1/fd/2)
+
+# Execute ilivalidator/ili2gpkg with the given options
+$JAVA_HOME/bin/java -jar $ILITOOLS_HOME_DIR/ili2c/$ILI2C_VERSION/ili2c.jar $options --check-repo-ilis $ILI2C_INPUT_DIR
+
+if [ $? -eq 0 ]
+then
+    echo "Repository validated sucessfully. Copying contents of '$ILI2C_INPUT_DIR' to '$ILI2C_OUTPUT_DIR'..."
+    rm -r $ILI2C_OUTPUT_DIR/* && cp -r $ILI2C_INPUT_DIR/* $ILI2C_OUTPUT_DIR
+    echo Validated: $(date +"%H:%M %d.%m.%Y") > $ILI2C_OUTPUT_DIR/Version.md
+    exit 0
+else
+    echo "Repository validation resulted in errors. Check logs above"
+    exit 1
+fi
